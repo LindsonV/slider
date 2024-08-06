7a1858303aea0f3861f0f89be81a7e25b1f50898
Lindson Viana <159596461+LindsonV@users.noreply.github.com>
2024-08-06 01:45:02 -0300

    slider-para-hamburgueria.

---
+++ script.js
@@ -0,0 +1,12 @@
+let next = document.querySelector('.Proximo')
+let prev = document.querySelector('.Voltar')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1])
+})
